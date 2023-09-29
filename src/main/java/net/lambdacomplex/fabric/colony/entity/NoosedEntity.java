b099dbe865185c9ddd8bdeaede2086c9189a760a
kultidgames <chrisl0078@gmail.com>
2023-09-29 19:00:24 -0400

    Noose Entity Rendered, Unworking

---
+++ src/main/java/net/lambdacomplex/fabric/colony/entity/NoosedEntity.java
@@ -0,0 +1,114 @@
+package net.lambdacomplex.fabric.colony.entity;
+
+import net.lambdacomplex.fabric.colony.entity.decoration.NooseKnotEntity;
+import net.lambdacomplex.fabric.colony.item.ModItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.network.packet.s2c.play.EntityAttachS2CPacket;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class NoosedEntity extends MobEntity {
+    @Nullable
+    private NbtCompound nooseNbt;
+    private Entity holdingEntity;
+
+    protected NoosedEntity(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.world.isClient) {
+            this.updateNoose();
+            if (this.age % 5 == 0) {
+                this.updateGoalControls();
+            }
+        }
+    }
+    public boolean canBeNoosedBy(PlayerEntity player) {
+        return !this.isNoosed() && !(this instanceof Monster);
+    }
+    public boolean isNoosed() {
+        return this.holdingEntity != null;
+    }
+    protected void updateNoose() {
+        if (this.nooseNbt != null) {
+            this.readNooseNbt();
+        }
+
+        if (this.holdingEntity != null) {
+            if (!this.isAlive() || !this.holdingEntity.isAlive()) {
+                this.detachNoose(true, true);
+            }
+
+        }
+    }
+
+    public void detachNoose(boolean sendPacket, boolean dropItem) {
+        if (this.holdingEntity != null) {
+            this.holdingEntity = null;
+            this.nooseNbt = null;
+            if (!this.world.isClient && dropItem) {
+                this.dropItem(Items.LEAD);
+            }
+
+            if (!this.world.isClient && sendPacket && this.world instanceof ServerWorld) {
+                ((ServerWorld)this.world).getChunkManager().sendToOtherNearbyPlayers(this, new EntityAttachS2CPacket(this, (Entity)null));
+            }
+        }
+    }
+
+    public void attachNoose(Entity entity, boolean sendPacket) {
+        this.holdingEntity = entity;
+        this.nooseNbt = null;
+        if (!this.world.isClient && sendPacket && this.world instanceof ServerWorld) {
+            ((ServerWorld)this.world).getChunkManager().sendToOtherNearbyPlayers(this, new EntityAttachS2CPacket(this, this.holdingEntity));
+        }
+        if (this.hasVehicle()) {
+            this.stopRiding();
+        }
+    }
+    private void readNooseNbt() {
+        if (this.nooseNbt != null && this.world instanceof ServerWorld) {
+            if (this.nooseNbt.containsUuid("UUID")) {
+                UUID uUID = this.nooseNbt.getUuid("UUID");
+                Entity entity = ((ServerWorld) this.world).getEntity(uUID);
+                if (entity != null) {
+                    this.attachNoose(entity, true);
+                    return;
+                }
+            } else if (this.nooseNbt.contains("X", 99) && this.nooseNbt.contains("Y", 99) && this.nooseNbt.contains("Z", 99)) {
+                BlockPos blockPos = NbtHelper.toBlockPos(this.nooseNbt);
+                this.attachNoose(NooseKnotEntity.getOrCreate(this.world, blockPos), true);
+                return;
+            }
+
+            if (this.age > 100) {
+                this.dropItem(ModItems.NOOSE);
+                this.nooseNbt = null;
+            }
+        }
+    }
+    @Override
+    protected void removeFromDimension() {
+        super.removeFromDimension();
+        this.detachNoose(true, false);
+        this.getItemsEquipped().forEach((stack) -> {
+            stack.setCount(0);
+        });
+    }
+
+
+}
--- src/main/java/net/lambdacomplex/fabric/colony/item/custom/NooseItem.java
+++ src/main/java/net/lambdacomplex/fabric/colony/item/custom/NooseItem.java
@@ -1,76 +1,35 @@
 package net.lambdacomplex.fabric.colony.item.custom;
 
+import net.lambdacomplex.fabric.colony.entity.NoosedEntity;
 import net.lambdacomplex.fabric.colony.entity.decoration.NooseKnotEntity;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
-import java.util.List;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class NooseItem extends Item {
-    public NooseItem(Settings settings) {super(settings);}
+    public NooseItem(Settings settings) {
+        super(settings);
+    }
+
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
-        BlockState blockState = world.getBlockState(blockPos);
-        PlayerEntity playerEntity = null;
-        if (blockState.isIn(BlockTags.FENCES)) {
-            playerEntity = context.getPlayer();
-            if (!world.isClient && playerEntity != null) {
-                playerEntity.sendMessage(Text.literal("bye"));
-                return attachHeldMobsToBlock(playerEntity, world, blockPos);
-            }
-
-            return ActionResult.success(world.isClient);
-        } else {
-        //    playerEntity.sendMessage(Text.literal("hi")); // Send a chat message to the player
-            return ActionResult.PASS;
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity player, LivingEntity entity, Hand hand) {
+        World world = player.getWorld();
+        NooseKnotEntity nooseKnotEntity = new NooseKnotEntity(player.getWorld(), player.getBlockPos());
+        if (!world.isClient) {
+            ((NoosedEntity) entity).attachNoose(nooseKnotEntity, true);
+            player.sendMessage(Text.literal("attached!"));
         }
+
+        return ActionResult.SUCCESS;
     }
-
-    public static ActionResult attachHeldMobsToBlock(PlayerEntity player, World world, BlockPos pos) {
-        player.sendMessage(Text.literal("attachHeldMobsToBlock called."), false); // Debug message
-        NooseKnotEntity leashKnotEntity = null;
-        boolean bl = false;
-        int i = pos.getX();
-        int j = pos.getY();
-        int k = pos.getZ();
-
-        List<MobEntity> list = world.getNonSpectatingEntities(MobEntity.class, new Box((double)i - 7.0, (double)j - 7.0, (double)k - 7.0, (double)i + 7.0, (double)j + 7.0, (double)k + 7.0));
-        for (MobEntity mobEntity : list) {
-            if (mobEntity.getHoldingEntity() == player) {
-                if (leashKnotEntity == null) {
-                    leashKnotEntity = (NooseKnotEntity) NooseKnotEntity.getOrCreate(world, pos);
-                    leashKnotEntity.onPlace();
-                    player.sendMessage(Text.literal("LeashKnotEntity created."), false); // Debug message
-                }
-
-                mobEntity.attachLeash(leashKnotEntity, true);
-                bl = true;
-                player.sendMessage(Text.literal("Leash attached to mob."), false); // Debug message
-            }
-        }
-
-        if (bl) {
-            world.emitGameEvent(GameEvent.BLOCK_ATTACH, pos, GameEvent.Emitter.of(player));
-            player.sendMessage(Text.literal("Block attached to mob."), false); // Debug message
-        } else {
-            player.sendMessage(Text.literal("No mobs found."), false); // Debug message
-        }
-
-        return bl ? ActionResult.SUCCESS : ActionResult.PASS;
-    }
-
-
 }
 
